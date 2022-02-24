91b15d24a88ac10bbbc4a34fa1c5a67d94aafbc8
dwaggoner24 <91644175+dwaggoner24@users.noreply.github.com>
2022-02-24 10:32:49 -0600

    Add files via upload

---
+++ src/components/components/PhotoContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Photo from './Photo';
+import NotFound from './NotFound';
+
+const PhotoContainer = (props) => {
+
+    const results = props.data
+    let photos;
+    if (results.length > 0) {
+        photos = results.map(photo => <Photo url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} key={photo.id} alt={`${photo.title}`}/>);
+    } else {
+        photos = <NotFound />
+    }
+
+    return (
+        <div className="photo-container">
+            <h2>Results</h2>
+                <ul>
+                    {photos}
+                </ul>
+            </div>
+    )
+}
+
+export default PhotoContainer; 
